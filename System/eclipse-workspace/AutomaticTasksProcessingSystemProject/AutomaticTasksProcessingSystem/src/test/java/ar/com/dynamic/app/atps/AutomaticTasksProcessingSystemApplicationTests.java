adfcc13965cac9d5b63c1a8b14966797b8e50e24
Adrián E. Córdoba <software.asia@gmail.com>
2024-03-14 11:11:36 -0300

    Company name changed.

---
+++ System/eclipse-workspace/AutomaticTasksProcessingSystemProject/AutomaticTasksProcessingSystem/src/test/java/ar/com/dynamic/app/atps/AutomaticTasksProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.dynamic.app.atps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticTasksProcessingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
