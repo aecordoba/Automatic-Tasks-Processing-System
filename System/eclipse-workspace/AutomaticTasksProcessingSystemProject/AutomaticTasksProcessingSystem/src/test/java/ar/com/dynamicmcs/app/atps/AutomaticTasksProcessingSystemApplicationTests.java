260c3bf6c9f41954dbbcaac532f4e096a7eee41a
Adrián E. Córdoba <software.asia@gmail.com>
2024-04-30 16:25:26 -0300

    Controllers in context test.

--- System/eclipse-workspace/AutomaticTasksProcessingSystemProject/AutomaticTasksProcessingSystem/src/test/java/ar/com/dynamicmcs/app/atps/AutomaticTasksProcessingSystemApplicationTests.java
+++ System/eclipse-workspace/AutomaticTasksProcessingSystemProject/AutomaticTasksProcessingSystem/src/test/java/ar/com/dynamicmcs/app/atps/AutomaticTasksProcessingSystemApplicationTests.java
@@ -1,13 +1,33 @@
 package ar.com.dynamicmcs.app.atps;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import ar.com.dynamicmcs.app.atps.web.controllers.EngineStateMonitorController;
+import ar.com.dynamicmcs.app.atps.web.controllers.LoginController;
+import ar.com.dynamicmcs.app.atps.web.controllers.SystemControlController;
+import ar.com.dynamicmcs.app.atps.web.controllers.UserRegisterController;
 
 @SpringBootTest
 class AutomaticTasksProcessingSystemApplicationTests {
+	@Autowired
+	private LoginController loginController;
+	@Autowired
+	private UserRegisterController userRegisterController;
+	@Autowired
+	private SystemControlController systemControlController;
+	@Autowired
+	private EngineStateMonitorController engineStateMonitorController;
 
 	@Test
 	void contextLoads() {
+		assertThat(loginController).isNotNull();
+		assertThat(userRegisterController).isNotNull();
+		assertThat(systemControlController).isNotNull();
+		assertThat(engineStateMonitorController).isNotNull();
 	}
 
 }
